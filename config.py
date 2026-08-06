7b5814a337fe27636f8b3e187dcae1e52487b11a
sandersbasket25-cpu <sandersbasket25@gmail.com>
2026-08-06 15:47:18 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 import os
 
-# Railway будет брать эти данные из раздела Variables
+# Railway Variables
 API_ID = int(os.getenv("API_ID", "0"))
 API_HASH = os.getenv("API_HASH", "")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 SESSION_STRING = os.getenv("SESSION_STRING", "")
 
-# Превращаем строку ID чата в число
+# ID чата (группы)
 CHAT = int(os.getenv("CHAT", "0"))
 
-# Превращаем строку админов "123,456" в список чисел [123, 456]
-ADMINS_STR = os.getenv("ADMINS", "")
-ADMINS = [int(x.strip()) for x in ADMINS_STR.split(",") if x.strip()]
+# Список админов (принимаем строку "123,456", превращаем в список [123, 456])
+admins_raw = os.getenv("ADMINS", "")
+ADMINS = [int(x.strip()) for x in admins_raw.split(",") if x.strip()]
 
 DOWNLOAD_DIR = "downloads"
 if not os.path.exists(DOWNLOAD_DIR):
