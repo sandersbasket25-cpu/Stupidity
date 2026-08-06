9f969d722272366f0a06b80909e80212e504fd44
sandersbasket25-cpu <sandersbasket25@gmail.com>
2026-08-06 16:14:19 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,11 @@
 import os
 
-# Railway Variables
 API_ID = int(os.getenv("API_ID", "0"))
 API_HASH = os.getenv("API_HASH", "")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 SESSION_STRING = os.getenv("SESSION_STRING", "")
 
-# ID чата (группы)
 CHAT = int(os.getenv("CHAT", "0"))
 
-# Список админов (принимаем строку "123,456", превращаем в список [123, 456])
 admins_raw = os.getenv("ADMINS", "")
 ADMINS = [int(x.strip()) for x in admins_raw.split(",") if x.strip()]
 
